6325ce7a73c77632fb30b3815eb783d0906cf6d9
Zino-ctrlZ <onokpisekome@gmail.com>
2025-10-12 18:29:38 -0500

    date type

--- EventDriven/types.py
+++ EventDriven/types.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, date
 from enum import Enum
 
 class ResultsEnum(Enum):
@@ -79,7 +79,7 @@ class Order:
     result: str
     signal_id: str
     map_signal_id: str
-    date: datetime
+    date: date
     data: OrderData
 
     def __getitem__(self, key):
