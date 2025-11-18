7e3562804071e1b527d151eb250c6e7fe04bc821
Chidifinance <142176703+Chidifinance@users.noreply.github.com>
2025-11-18 01:41:37 -0500

    Clean up

--- EventDriven/dataclasses/orders.py
+++ EventDriven/dataclasses/orders.py
@@ -22,20 +22,3 @@ class OrderRequest:
     option_type: Literal["c", "p"]
     spot: numbers.Number = None
     chain_spot: numbers.Number = None
-
-    
-
-
-# @pydantic_dataclass(config=ConfigDict(arbitrary_types_allowed=True))
-# class RiskManagerOrderRequest:
-#     """
-#     Dataclass representing an order request generated for risk manager to process.
-#     """
-
-#     symbol: str
-#     date: Union[datetime, str]
-#     signal_id: str
-#     max_close: numbers.Number
-#     direction: str
-#     tick_cash: numbers.Number
-#     option_type: str
