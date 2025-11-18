7aa61576347524ff67ffbf1fcfa6835c1497bc94
Chidifinance <142176703+Chidifinance@users.noreply.github.com>
2025-11-18 01:56:22 -0500

    Extending AtTimePositiondata for specific price. Plus Base handling

--- EventDriven/dataclasses/timeseries.py
+++ EventDriven/dataclasses/timeseries.py
@@ -4,22 +4,49 @@ import numbers
 from pydantic.dataclasses import dataclass as pydantic_dataclass
 from pydantic import ConfigDict, Field
 
-@pydantic_dataclass(config=ConfigDict(arbitrary_types_allowed=True))
-class AtTimeOptionData:
+
+@pydantic_dataclass(config=ConfigDict(arbitrary_types_allowed=True), kw_only=True)
+class AtTimeBaseData:
+    """
+    Base dataclass for at-time data representations.
+    This is solely for market data at a specific time for options and positions.
+    """
+
+    date: Union[datetime, str]
+    close: numbers.Number
+    bid: numbers.Number
+    ask: numbers.Number
+    midpoint: numbers.Number
+    delta: numbers.Number
+    gamma: numbers.Number
+    vega: numbers.Number
+    theta: numbers.Number
+
+    ## USE_{ITEMS} for choosing btwn bid/ask/close/midpoint
+    use_price: str = "midpoint"
+
+    def get_price(self) -> numbers.Number:
+        """
+        Get the price based on the specified use_price attribute.
+        """
+        if self.use_price == "close":
+            return self.close
+        elif self.use_price == "bid":
+            return self.bid
+        elif self.use_price == "ask":
+            return self.ask
+        elif self.use_price == "midpoint":
+            return self.midpoint
+        else:
+            raise ValueError(f"Invalid use_price value: {self.use_price}")
+
+
+@pydantic_dataclass(config=ConfigDict(arbitrary_types_allowed=True), kw_only=True)
+class AtTimeOptionData(AtTimeBaseData):
     opttick: str
-    date: Union[datetime, str]
-    close: numbers.Number
-    bid: numbers.Number
-    ask: numbers.Number
-    midpoint: numbers.Number
 
 
-@pydantic_dataclass(config=ConfigDict(arbitrary_types_allowed=True))
-class AtTimePositionData:
+@pydantic_dataclass(config=ConfigDict(arbitrary_types_allowed=True), kw_only=True)
+class AtTimePositionData(AtTimeBaseData):
     position_id: str
-    date: Union[datetime, str]
-    close: numbers.Number
-    bid: numbers.Number
-    ask: numbers.Number
-    midpoint: numbers.Number
     skips: Dict[str, Any] = Field(default_factory=dict)
