5255edde40dce56783bfcc57d6adc0e43f2173d7
Chidifinance <142176703+Chidifinance@users.noreply.github.com>
2026-04-05 20:16:00 -0400

    QuantTools: remove obsolete EventDriven testing artifacts

--- EventDriven/Testing/cron_test.py
+++
@@ -1,4 +0,0 @@
-from datetime import datetime
-
-with open('/Users/chiemelienwanisobi/cloned_repos/QuantTools/EventDriven/Testing/text.txt', 'a') as f:
-    f.write(f"Current time is {datetime.now()}\n")
